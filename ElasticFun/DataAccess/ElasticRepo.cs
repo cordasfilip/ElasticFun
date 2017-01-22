e784d10386d4b98645e91fdc4136bea0c259a4c4
RiperZR <RiperZR@DESKTOP-O7J3KNN>
2017-01-22 17:41:49 +0100

    Added data load

---
+++ ElasticFun/DataAccess/ElasticRepo.cs
@@ -0,0 +1,61 @@
+﻿using CsvHelper;
+using ElasticFun.Models;
+using Nest;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElasticFun.DataAccess
+{
+    public class ElasticRepo
+    {
+        private readonly ElasticClient client;
+
+        public ElasticRepo()
+        {
+            var uri = "http://localhost:9200";
+            client = new ElasticClient(new Uri(uri));
+        }
+
+        public async Task AddDataAsync(string indexName)
+        {
+            var path = @"C:\Users\RiperZR\Source\Repos\ElasticFun\ElasticFun\Data\companylist.csv";
+            foreach (var company in LoadCompany(path))
+            {
+                await client.IndexAsync(company,id=>id.Index(indexName));
+            }
+        }
+
+        public async Task<IEnumerable<Index>> GetAllIndex()
+        {
+            var mapping = await client.LowLevel.IndicesGetMappingForAllAsync<JObject>();
+
+            return Enumerable.Empty<Index>();
+        }
+
+
+        public IEnumerable<Company> LoadCompany(string location)
+        {
+            var csv = new CsvReader(new StreamReader(File.OpenRead(location)));
+            while (csv.Read())
+            {
+                yield return new Company
+                {
+                    Symbol = csv.GetField<string>("Symbol"),
+                    Name = csv.GetField<string>("Name"),
+                    Industry = csv.GetField<string>("Name"),
+                    IPOyear = csv.GetField<string>("IPOyear"),
+                    LastSale = csv.GetField<double?>("LastSale"),
+                    MarketCap = csv.GetField<double?>("MarketCap"),
+                    Sector = csv.GetField<string>("Sector"),
+                    SummaryQuote = csv.GetField<string>("Sector")
+                };
+            }
+
+        }
+    }
+}
---
+++ ElasticFun/Models/Company.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElasticFun.Models
+{
+    public class Company
+    {
+        //Symbol","Name","LastSale","MarketCap","ADR TSO","IPOyear","Sector","Industry","Summary Quote"
+        public string Symbol { get; set; }
+
+        public string Name { get; set; }
+
+        public double? LastSale { get; set; }
+
+        public double? MarketCap { get; set; }
+
+        public string IPOyear { get; set; }
+
+        public string Sector { get; set; }
+
+        public string Industry { get; set; }
+
+        public string SummaryQuote { get; set; }
+    }
+}
